b494d37d5adaf30bee3dd4ed8cc8b670a778325f
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-06 19:55:06 -0300

    JS preencher usando data-bind

--- js/cartao.js
+++ js/cartao.js
@@ -1,15 +1,21 @@
 ;(() => {
-  const cartaoPreencherNumero = () => {
-    const $campoNumero = document.querySelector('.js-cartao-campo-numero')
-    const $numeros     = document.querySelector('.js-cartao-numeros')
-  
-    $campoNumero.addEventListener('input', () => {
-      $numeros.textContent = inserirEspaco(event.currentTarget.value)
+  const cartaoPreencher = () => {
+    const $campos = document.querySelectorAll('.js-cartao-data-bind')
+
+    $campos.forEach($campo => {
+      $campo.addEventListener('input', () => preencher(event))
     })
+
+    const preencher = () => {
+      const alvo  = event.currentTarget.dataset.bind        
+      const $alvo = document.querySelector(`.js-cartao-data-bind-alvo[data-bind=${alvo}]`)
+
+      $alvo.textContent = inserirEspacos(event.currentTarget.value)
+    }
   }
-  
-  cartaoPreencherNumero()
+
+  cartaoPreencher()
 })()
 
 
-const inserirEspaco = valor => valor.replace(/(\d{4})/g, '$1 ').trim()
+const inserirEspacos = alvo => alvo.replace(/(\d{4})/g, '$1 ').trim()
