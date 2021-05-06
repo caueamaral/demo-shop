b573728fb62692fb8f206acb7d4412cf25aed027
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-06 13:25:15 -0300

    Cartão frente

--- js/cartao.js
+++ js/cartao.js
@@ -1,9 +1,11 @@
-const cartaoNumero = () => {
-  const $campoNumero = document.querySelector('.js-cartao-numero')
-
-  $campoNumero.addEventListener('input', () => {
-    console.log(event.currentTarget.value)
-  })
-}
-
-cartaoNumero()
+;(() => {
+  const cartaoNumero = () => {
+    const $campoNumero = document.querySelector('.js-cartao-numero')
+  
+    $campoNumero.addEventListener('input', () => {
+      console.log(event.currentTarget.value)
+    })
+  }
+  
+  cartaoNumero()
+})()
