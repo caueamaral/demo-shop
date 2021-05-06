823780c081e4c39c7d5ad98032555f72a038fdec
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-06 13:13:33 -0300

    JS estrutura inicial

---
+++ js/cartao.js
@@ -0,0 +1,9 @@
+const cartaoNumero = () => {
+  const $campoNumero = document.querySelector('.js-cartao-numero')
+
+  $campoNumero.addEventListener('input', () => {
+    console.log(event.currentTarget.value)
+  })
+}
+
+cartaoNumero()
