69d0c2ebf52427e318998d6db1b87b6fd975eba7
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-06 20:07:27 -0300

    JS preencher com a lógica melhorada

--- js/cartao.js
+++ js/cartao.js
@@ -4,13 +4,18 @@
 
     $campos.forEach($campo => {
       $campo.addEventListener('input', () => preencher(event))
+      $campo.addEventListener('input', () => formatar(event))
     })
 
     const preencher = () => {
-      const alvo  = event.currentTarget.dataset.bind        
+      const alvo  = event.target.dataset.bind        
       const $alvo = document.querySelector(`.js-cartao-data-bind-alvo[data-bind=${alvo}]`)
 
-      $alvo.textContent = inserirEspacos(event.currentTarget.value)
+      $alvo.textContent = formatarEspacos(event.target.value)
+    }
+
+    const formatar = () => {
+      event.target.value = formatarEspacos(event.target.value.trim())
     }
   }
 
@@ -18,4 +23,9 @@
 })()
 
 
-const inserirEspacos = alvo => alvo.replace(/(\d{4})/g, '$1 ').trim()
+const formatarEspacos = alvo => {
+  return alvo
+    .replace(/ +/g, '')
+    .replace(/(\d{4})/g, '$1 ')
+    .trim()
+}
