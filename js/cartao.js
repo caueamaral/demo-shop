a10794cac2653c8c16065ba651513cc334b79e2a
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-06 20:33:12 -0300

    Cartão com nome em 2 linhas

--- js/cartao.js
+++ js/cartao.js
@@ -10,12 +10,21 @@
     const preencher = () => {
       const alvo  = event.target.dataset.bind        
       const $alvo = document.querySelector(`.js-cartao-data-bind-alvo[data-bind=${alvo}]`)
+      let valor   = event.target.value
 
-      $alvo.textContent = formatarEspacos(event.target.value)
+      if (alvo == 'numero') {
+        valor = formatarEspacos(valor)
+      }
+
+      $alvo.textContent = valor
     }
 
     const formatar = () => {
-      event.target.value = formatarEspacos(event.target.value.trim())
+      const alvo = event.target.dataset.bind
+
+      if (alvo == 'numero') {
+        event.target.value = formatarEspacos(event.target.value.trim())
+      }
     }
   }
 
