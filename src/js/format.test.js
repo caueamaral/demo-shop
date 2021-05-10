5d4a9e11fa8da9424892020bcec1dfbf983d7495
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-09 22:49:10 -0300

    Jest: barInsertion()

--- src/js/format.test.js
+++ src/js/format.test.js
@@ -1,4 +1,4 @@
-import { spacesInsertion, removeLetters, removeNumbers } from './format'
+import { spacesInsertion, removeLetters, removeNumbers, barInsertion } from './format'
 
 test('insert spaces between every 4 numbers', () => {
   expect(spacesInsertion('1234123412341234')).toBe('1234 1234 1234 1234')
@@ -10,4 +10,8 @@ test('remove all the letters, spaces and special characters', () => {
 
 test('remove all numbers', () => {
   expect(removeNumbers('1text2')).toBe('text')
+})
+
+test('insert bar after 2 numbers and allows up to 4 numbers', () => {
+  expect(barInsertion('1234')).toBe('12/34')
 })
