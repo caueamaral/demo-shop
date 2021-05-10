186c775379df31980ae7f1a8ac7d8e4cffadd200
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-09 22:42:46 -0300

    Jest: removeNumbers()

--- src/js/format.test.js
+++ src/js/format.test.js
@@ -1,4 +1,4 @@
-import { spacesInsertion, removeLetters } from './format'
+import { spacesInsertion, removeLetters, removeNumbers } from './format'
 
 test('insert spaces between every 4 numbers', () => {
   expect(spacesInsertion('1234123412341234')).toBe('1234 1234 1234 1234')
@@ -6,4 +6,8 @@ test('insert spaces between every 4 numbers', () => {
 
 test('remove all the letters, spaces and special characters', () => {
   expect(removeLetters('! 12 text 34 ?')).toBe('1234')
+})
+
+test('remove all numbers', () => {
+  expect(removeNumbers('1text2')).toBe('text')
 })
